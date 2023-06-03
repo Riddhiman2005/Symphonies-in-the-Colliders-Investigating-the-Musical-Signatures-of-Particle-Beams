2d9d610683276894a44a8553711fc47446bc497f
Riddhi'sChaoticWorld <130882317+Riddhiman2005@users.noreply.github.com>
2023-06-03 21:48:32 +0530

    Update Musical notes.py

--- Musical notes.py
+++ Musical notes.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from scipy.io.wavfile import write
 
 # Particle properties
+
 electron_charge = 1.6e-19      # Charge of electron (C)
 proton_charge = 1.6e-19        # Charge of proton (C)
 neutron_charge = 0             # Charge of neutron (C)
@@ -12,30 +13,35 @@ proton_mass = 1.67e-27         # Mass of proton (kg)
 neutron_mass = 1.67e-27        # Mass of neutron (kg)
 
 # Magnetic field properties
+
 B0 = 1.0         # Strength of magnetic field at z=0 (T)
 dBdz_factor = 1e-4      # Scaling factor for the rate of change of magnetic field with respect to z (T/m)
 z_threshold = 1.0   # Z-coordinate threshold for hitting the surface (m)
 
 # Time properties
+
 t0 = 0           # Initial time (s)
 tmax = 1e-7      # Maximum simulation time (s)
 dt = 1e-12       # Time step (s)
 N = int(tmax/dt) # Number of time steps
 
 # Function to calculate the magnetic field at a given z-coordinate based on particle properties
+
 def magnetic_field_z(z, particle_charge, particle_mass):
     dBdz = dBdz_factor * particle_charge / particle_mass
     return B0 + dBdz * z
 
 # Function to calculate the Lorentz force
+
 def lorentz(q, v, B):
     return q * np.cross(v, B)
 
 # Function to calculate the acceleration of the particle
+
 def acceleration(q, v, B, m):
     return lorentz(q, v, B) / m
 
-# Initialize arrays to store the particle's position and velocity
+# Initializing arrays to store the particle's position and velocity
 x = np.zeros(N)
 y = np.zeros(N)
 z = np.zeros(N)
@@ -44,6 +50,7 @@ vy = np.zeros(N)
 vz = np.zeros(N)
 
 # Set the initial position and velocity of the particle
+
 x[0] = 0
 y[0] = 0
 z[0] = 0
@@ -51,7 +58,8 @@ vx[0] = 1e6
 vy[0] = 0
 vz[0] = 0
 
-# Use the fourth-order Runge-Kutta method to solve the equations of motion
+# Using Fourth-order Runge-Kutta method to solve the equations of motion
+
 for i in range(1, N):
     t = t0 + i * dt
     if z[i-1] < z_threshold:
@@ -80,7 +88,8 @@ for i in range(1, N):
     y[i] = y[i-1] + (k1x[1] + 2*k2x[1] + 2*k3x[1] + k4x[1]) / 6
     z[i] = z[i-1] + (k1x[2] + 2*k2x[2] + 2*k3x[2] + k4x[2]) / 6
 
-# Detect particles and generate musical notes
+# Detecting particles and generate musical notes
+
 notes = []
 for i in range(N):
     if z[i] >= z_threshold:
@@ -91,7 +100,8 @@ for i in range(N):
         elif charge == neutron_charge:
             notes.append('G')
 
-# Plot the trajectory
+# Plotting the trajectory
+
 plt.figure()
 plt.plot(x, z)
 plt.xlabel('x (m)')
@@ -100,7 +110,8 @@ plt.title('Trajectory of Charged Particles')
 plt.grid(True)
 plt.show()
 
-# Save the musical notes as a WAV file
+# Saving the musical notes as a WAV file
+
 sampling_rate = 44100
 duration = tmax
 t = np.linspace(0, duration, int(sampling_rate * duration))
